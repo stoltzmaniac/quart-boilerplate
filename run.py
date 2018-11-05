2556b24e6ba1023fdeda1d265ec7917216221684
stoltzmaniac <scott@stoltzmanconsulting.com>
2018-11-05 15:00:31 -0700

    Add websocket example

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from quart import Quart, jsonify
+from quart import Quart, jsonify, websocket
 from api.views import api
 
 
@@ -9,6 +9,13 @@ app.register_blueprint(api, url_prefix='/api')
 @app.route('/')
 def index():
     return jsonify({'Index': 'Page'})
+
+
+@app.websocket('/ws')
+async def ws():
+    while True:
+        data = await websocket.receive()
+        await websocket.send(f"echo {data}")
 
 
 if __name__ == '__main__':
